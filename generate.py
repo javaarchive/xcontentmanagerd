4ef8922bbb2f361997f1c59885ddd0d5af4d4897
Raymond <20248577+javaarchive@users.noreply.github.com>
2024-08-06 22:14:49 -0700

    dns modification nonworking

--- generate.py
+++ generate.py
@@ -7,7 +7,11 @@ import os
 import requests
 
 HAS_LIMITED_INTERNET = False
+if os.getenv("HAS_LIMITED_INTERNET", None):
+    HAS_LIMITED_INTERNET = True
 CF_API_KEY = os.getenv("CF_API_KEY", None)
+CF_ZONE_ID = os.getenv("CF_ZONE_ID", None)
+CF_DNS_ID = os.getenv("CF_DNS_ID", None)
 if len(sys.argv) < 2:
     print("Usage: python3 generate.py <ip on network interface to use>")
     sys.exit(1)
@@ -39,7 +43,19 @@ print("Wrote configs...")
 if HAS_LIMITED_INTERNET and not os.getenv("SKIP_DNS_UPDATE", False):
     # use cloudflare dns to update dns records
     if CF_API_KEY:
-        requests.post()
+        print("Updating DNS records...")
+        response = requests.patch(f"https://api.cloudflare.com/client/v4/zones/{CF_ZONE_ID}/dns_records/{CF_DNS_ID}", json = {
+            "content": host,
+            "type": "A",
+            "name": "intra.smashcloud.org",
+            "proxied": False,
+            "comment": "Automatically added by xcontentmanagerd",
+            "ttl": 86400 # cache as much as possible in case internet die
+        }, headers = {
+            "X-Auth-Email": os.getenv("CF_EMAIL", None),
+            "X-Auth-Key": CF_API_KEY,
+        })
+        print("Updated DNS records:", response.json())
     else:
         print("SKIP_DNS_UPDATE is set, but no CF_API_KEY is set. skipping dns update.")
 
