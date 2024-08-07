eede6f9e55e1edb171e06ff78a7538e8fa9f098f
Raymond <20248577+javaarchive@users.noreply.github.com>
2024-08-06 22:48:11 -0700

    fix dns record updater to auth correctly

--- generate.py
+++ generate.py
@@ -44,18 +44,34 @@ if HAS_LIMITED_INTERNET and not os.getenv("SKIP_DNS_UPDATE", False):
     # use cloudflare dns to update dns records
     if CF_API_KEY:
         print("Updating DNS records...")
-        response = requests.patch(f"https://api.cloudflare.com/client/v4/zones/{CF_ZONE_ID}/dns_records/{CF_DNS_ID}", json = {
-            "content": host,
-            "type": "A",
-            "name": "intra.smashcloud.org",
-            "proxied": False,
-            "comment": "Automatically added by xcontentmanagerd",
-            "ttl": 86400 # cache as much as possible in case internet die
-        }, headers = {
-            "X-Auth-Email": os.getenv("CF_EMAIL", None),
-            "X-Auth-Key": CF_API_KEY,
+        print("Get existing dns records...")
+        response = requests.get(f"https://api.cloudflare.com/client/v4/zones/{CF_ZONE_ID}/dns_records", headers = {
+            # "X-Auth-Email": os.getenv("CF_EMAIL", None),
+            "Authorization": f"Bearer {CF_API_KEY}",
         })
-        print("Updated DNS records:", response.json())
+        result = response.json()
+        if result["success"]:
+            records = result["result"]
+            print("Got existing dns records count:", len(records))
+            dns_id = CF_DNS_ID
+            for record in records:
+                if record["name"] == "intra.smashcloud.org":
+                    dns_id = record["id"]
+                    print("Found existing dns record with id", dns_id, " ignoring env var...", CF_DNS_ID)
+                    break
+            response = requests.patch(f"https://api.cloudflare.com/client/v4/zones/{CF_ZONE_ID}/dns_records/{dns_id}", json = {
+                "content": host,
+                "type": "A",
+                "name": "intra.smashcloud.org",
+                "proxied": False,
+                "comment": "Automatically added by xcontentmanagerd",
+                "ttl": 86400 # cache as much as possible in case internet die
+            }, headers = {
+                "Authorization": f"Bearer {CF_API_KEY}",
+            })
+            print("Updated DNS records:", response.json())
+        else:
+            print("Failed to get existing dns records:", result)
     else:
         print("SKIP_DNS_UPDATE is set, but no CF_API_KEY is set. skipping dns update.")
 
