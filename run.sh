c186c3c410d86fbf43aa6fd1cdc5a623af1090ba
Raymond <20248577+javaarchive@users.noreply.github.com>
2024-08-06 23:14:37 -0700

    fix calling again

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 # TODO arguments may not be needed?
 # but being specific is fine
-sudo caddy.exe run --config Caddyfile --adapter caddyfile
+sudo .\caddy.exe run --config Caddyfile --adapter caddyfile
